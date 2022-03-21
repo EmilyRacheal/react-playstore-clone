cd47fa48b6cdbae152a6395be09a7a34bf30c144
EmilyRacheal <emilychealy4@gmail.com>
2022-03-21 17:09:41 +0100

    add nav

--- src/components/pages/Dashboard/layout/TopMenu/index.jsx
+++ src/components/pages/Dashboard/layout/TopMenu/index.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import "./topMenu.css"
+import GridViewIcon from '@mui/icons-material/GridView';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function index() {
   return (
-    <div>index</div>
+    <div className='flex items-center justify-center w-full h-12 border border-blue-400 '>
+
+      <div className='w-1/5 h-12 bg-lime-600'>
+        <span className='flex items-center w-full px-6 py-2 text-xl text-white '>
+          <span className=''> <GridViewIcon /> </span>
+          <span className='ml-4'>Apps</span>
+        </span>
+      </div>
+
+      <div className='flex justify-between w-4/5 h-10 px-6 border'>
+
+        <div className='pt-2'>
+          <ul className='flex '>
+            <li className='pr-3 mx-3 home'>Category</li>
+            <li className='mx-3'>Home</li>
+            <li className='mx-3'>Top charts</li>
+            <li className='mx-3'>New releases</li>
+          </ul>
+        </div>
+
+        <div className='flex items-center justify-center'>
+          <span className='border border-gray-400 icon1'> <ContactSupportIcon /> </span>
+          <span className='border border-gray-400 icon2'> <SettingsIcon /> </span>
+        </div>
+
+      </div>
+    </div>
   )
 }
+
 
 export default index
