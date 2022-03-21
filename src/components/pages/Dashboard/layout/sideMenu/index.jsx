9484793cb4845c6701fb92879856903a4e56c956
EmilyRacheal <emilychealy4@gmail.com>
2022-03-21 17:50:19 +0100

    changed color

--- src/components/pages/Dashboard/layout/sideMenu/index.jsx
+++ src/components/pages/Dashboard/layout/sideMenu/index.jsx
@@ -13,14 +13,14 @@ const links = [
 
 function index() {
   return (
-    <div className=''>
-      <div className=''>
-        <ul>
-          <li>My apps</li>
-          <li>Shop</li>
+    <div className='w-1/5 border border-blue-600'>
+      <div className='mx-10'>
+        <ul className=''>
+          <li className='my-2'>My apps</li>
+          <li className='my-2'>Shop</li>
         </ul>
 
-        <div></div>
+        <div className='border-blue-600 boder'></div>
 
         <ul>
           <li>Games</li>
@@ -34,11 +34,11 @@ function index() {
         <ul>
           {
             links.map((item, i)=>{
-return (
-<li>{item.name}</li>
-)   })
+              return (
+                <li>{item.name}</li>
+              )   
+            })
           }
-
         </ul>
       </div>
     </div>
