6901e8f3fa5e099762874b0e460c2fbccee57594
EmilyRacheal <emilychealy4@gmail.com>
2022-03-22 16:35:09 +0100

    create data base

---
+++ src/jsonData/gamesData.jsx
@@ -0,0 +1,28 @@
+import faceLogo from "../assest/download.png"
+import whatsappLogo from "../assest/whatsapp.webp"
+import facebookLogo from "../assest/facebook.webp"
+import whatasspBusiness from "../assest/businesswhatsapp.webp"
+import netflix from "../assest/netflix.webp"
+import messanger from "../assest/messager.webp"
+import snapchat from "../assest/snapchat.webp"
+import spotify from "../assest/spotify.webp"
+import shazam from "../assest/shazam.webp"
+import googleChat from "../assest/googlechat.webp"
+import truecaller from "../assest/truecaller.webp"
+import luno from "../assest/luno.webp"
+
+
+export const gameData = [
+    {logo: faceLogo, name: "Facebook Lite", company: "Meta Platforms Inc", stars: 4},
+    {logo: whatsappLogo, name: "WhatsApp Messanger", company: "WhatsApp LLC", stars: 4},
+    {logo: facebookLogo, name: "Facebook", company: "Meta Platforms Inc", stars: 4},
+    {logo: whatasspBusiness, name: "WhatsApp Business", company: "WhatsApp LLC", stars: 4},
+    {logo: netflix, name: "Netflix", company: "Netflix Inc", stars: 4},
+    {logo: luno, name: "Luno", company: "Luno", stars: 4},
+    {logo: messanger, name: "Messanger Lite", company: "Meta Platforms Inc", stars: 4},
+    {logo: snapchat, name: "Snapchat", company: "Snaps Inc", stars: 4},
+    {logo: spotify, name: "Spotify: Music and", company: "Spotify AB", stars: 4},
+    {logo: shazam, name: "Shazam: Music Idea", company: "Apple Inc", stars: 4},
+    {logo: googleChat, name: "Google chat", company: "Google LLc", stars: 4},
+    {logo: truecaller, name: "Truecaller: ID & Space ", company: "Truecaller", stars: 4},
+]
