666186e4a5597e926a25e2532a40ead00afeba34
EmilyRacheal <emilychealy4@gmail.com>
2022-03-29 16:00:55 +0100

    create the playstore hompage

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React from "react";
 import "./card.css";
-import FacebookIcon from "../../assest/download.png"
-import GradeIcon from '@mui/icons-material/Grade';
+import FacebookIcon from "../../assest/download.png";
+import GradeIcon from "@mui/icons-material/Grade";
 
-function index() {
+function index({ logo, name, company }) {
   return (
-    <div className=''>
-        <div className='bg-white border card1'>
-          <img alt="logo" src={FacebookIcon} className="logo" />
-          <div className='py-3 ml-3 '>
-            <p>Facebook Lite Meta Platforms,</p> 
-            <span className='pb-3'> <GradeIcon sx={{ fontSize: 15}} /> </span>
-            <span> <GradeIcon sx={{ fontSize: 15 }}/> </span>
-            <span> <GradeIcon sx={{ fontSize: 15 }}/> </span>
-            <span> <GradeIcon sx={{ fontSize: 15 }}/> </span> 
-            <span> <GradeIcon sx={{ fontSize: 15 }} color="disabled" /> </span>  
-          </div>
-          
+    <div className="">
+      <div className="p-3 bg-white border card1">
+        <img alt="logo" src={logo} className="logo" />
+        <div className="py-3 ml-3 ">
+          <p className="name">{name}</p>
+          <span className="pb-3">
+            {" "}
+            <GradeIcon sx={{ fontSize: 15 }} />{" "}
+          </span>
+          <span>
+            {" "}
+            <GradeIcon sx={{ fontSize: 15 }} />{" "}
+          </span>
+          <span>
+            {" "}
+            <GradeIcon sx={{ fontSize: 15 }} />{" "}
+          </span>
+          <span>
+            {" "}
+            <GradeIcon sx={{ fontSize: 15 }} />{" "}
+          </span>
+          <span>
+            {" "}
+            <GradeIcon sx={{ fontSize: 15 }} color="disabled" />{" "}
+          </span>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
--- src/components/pages/Dashboard/home/index.jsx
+++ src/components/pages/Dashboard/home/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import { gameData } from "../../../../dummyData/gamesData";
+import Card from "../../../card/index";
+
 const Index = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="box-border p-7">
+      <div className="flex justify-between">
+        <h1>Popular Gamees and Apps</h1>
+        <button>See More</button>
+      </div>
+      <div className="flex flex-wrap w-full gap-5">
+        {gameData.map((item, i) => {
+          return (
+            <Card logo={item.logo} name={item.name} company={item.company} />
+          );
+        })}
+      </div>
     </div>
   );
 };
