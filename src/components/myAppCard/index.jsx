4608599ad9b1a60c5ceaebc49e6f2722e3979dc7
EmilyRacheal <emilychealy4@gmail.com>
2022-03-30 17:08:09 +0100

    add box shadow

--- src/components/myAppCard/index.jsx
+++ src/components/myAppCard/index.jsx
@@ -3,50 +3,42 @@ import "./myAppCard.css";
 import GoogleDrive from "../../assest/googledrive.webp";
 import GradeIcon from "@mui/icons-material/Grade";
 import CheckIcon from "@mui/icons-material/Check";
+import { color } from "@mui/system";
 
 const Index = () => {
   return (
     <div className="">
-      <div className="flex py-4 bg-white border border-yellow-400 card-width">
+      <div className="flex py-4 bg-white card-width">
         <div className="">
-          <img
-            alt="logo"
-            src={GoogleDrive}
-            className="border border-green-400 "
-          />
+          <img alt="logo" src={GoogleDrive} className="" />
         </div>
 
-        <div className="g-drive ">
-          <h1>Google Drive</h1>
-          <h4>Google LLC</h4>
-          <p className="mt-2 text-sm">
+        <div className="ml-5 g-drive">
+          <h1 className="">Google Drive</h1>
+          <h6>Google LLC</h6>
+          <p className="mt-4 text-sm">
             Store, access, and share securely with Google drive, part of Google
           </p>
-          <div className="flex ">
-            <div className="pt-8">
-              <span className="pb-3">
-                <GradeIcon sx={{ fontSize: 15 }} />
-              </span>
-              <span>
-                <GradeIcon sx={{ fontSize: 15 }} />
-              </span>
-              <span>
-                <GradeIcon sx={{ fontSize: 15 }} />
-              </span>
-              <span>
-                {" "}
-                <GradeIcon sx={{ fontSize: 15 }} />{" "}
-              </span>
-              <span>
-                <GradeIcon sx={{ fontSize: 15 }} color="disabled" />
-              </span>
+          <div className="pt-8">
+            <span className="pb-3 text-gray-500">
+              <GradeIcon sx={{ fontSize: 15 }} />
+            </span>
+            <span className="text-gray-500">
+              <GradeIcon sx={{ fontSize: 15 }} />
+            </span>
+            <span className="text-gray-500">
+              <GradeIcon sx={{ fontSize: 15 }} />
+            </span>
+            <span className="text-gray-500">
+              <GradeIcon sx={{ fontSize: 15 }} />{" "}
+            </span>
+            <span>
+              <GradeIcon sx={{ fontSize: 15 }} color="disabled" />
+            </span>
 
-              <span className="ml-10">
-                <CheckIcon />
-              </span>
-            </div>
-
-            <div></div>
+            <span className="ml-10 text-center text-white round-check bg-lime-400">
+              <CheckIcon sx={{ fontSize: 15 }} />
+            </span>
           </div>
         </div>
       </div>
