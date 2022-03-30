94a850b7161fa8ce70497c7da42db3474187c0d9
EmilyRacheal <emilychealy4@gmail.com>
2022-03-30 19:25:24 +0100

    add datas

--- src/secondDummy/myAppsData.jsx
+++ src/secondDummy/myAppsData.jsx
@@ -1,12 +1,47 @@
 import GoogleDrive from "../assest/googledrive.webp";
+import Xender from "../assest/xender.webp";
+import AudioMark from "../assest/audiomark.webp";
+import Boomplay from "../assest/boom.webp";
+import Chrome from "../assest/chrome.webp";
 
 export const myData = [
   {
     logo: GoogleDrive,
-    name: "Google Drive Google LLC",
+    name: "Google Drive ",
+    title: "Google LLC",
     description:
       "Store, access, and share securely with Google Drive, part of Google",
     stars: 4,
   },
-  {},
+  {
+    logo: Xender,
+    name: "Xender - Share Music Transfer",
+    title: "Xender File Sharing Team",
+    description:
+      "Share Music,share Video,Share Photo,Share It,Share Movie,File Sharing",
+    stars: 4,
+  },
+  {
+    logo: AudioMark,
+    name: "Audiomac: Music",
+    title: "Audiomack",
+    description:
+      "Stream the latest music & podcasts and listen offline. Play your local",
+    stars: 4,
+  },
+  {
+    logo: Boomplay,
+    name: "BoomPlay:Music",
+    title: "Transsnet music Limited",
+    description:
+      "A music downloader that allows you play 75m+ MP3 songs offline.",
+    stars: 4,
+  },
+  {
+    logo: Chrome,
+    name: "Google Chrome",
+    title: "Google LLC",
+    description:
+      "Fast, simple, and secure. Google Chrome browser for Android phones and tablets.",
+  },
 ];
