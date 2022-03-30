dd8463dd229d2dc310fcf8b3fef9f20d2e3fc855
EmilyRacheal <emilychealy4@gmail.com>
2022-03-30 19:05:09 +0100

    add pages

---
+++ src/secondDummy/myAppsData.jsx
@@ -0,0 +1,12 @@
+import GoogleDrive from "../assest/googledrive.webp";
+
+export const myData = [
+  {
+    logo: GoogleDrive,
+    name: "Google Drive Google LLC",
+    description:
+      "Store, access, and share securely with Google Drive, part of Google",
+    stars: 4,
+  },
+  {},
+];
