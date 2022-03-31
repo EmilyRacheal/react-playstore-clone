129596cbfb24ec20de7122e3572cec8549c9bb22
EmilyRacheal <emilychealy4@gmail.com>
2022-03-31 15:31:36 +0100

    add heading

--- src/components/gameCard/index.jsx
+++ src/components/gameCard/index.jsx
@@ -2,20 +2,37 @@ import React from "react";
 import "./gameCard.css";
 import KidsGame from "../../assest/games.webp";
 import Avatar from "../../assest/avatar.webp";
+import GradeIcon from "@mui/icons-material/Grade";
 
 const Index = () => {
   return (
     <div className="">
       <div className="game-width">
         <img alt="logo" src={KidsGame} className="game-img " />
-        <div className="flex flex-wrap px-4 py-3 bg-white">
+        <div className="flex flex-wrap px-4 py-3 bg-white line">
           <div>
-            {" "}
             <img alt="logo" src={Avatar} className="w-12" />
           </div>
           <div className="ml-2">
-            <h3>Guardain Tales</h3>
+            <h3 className="gaurd">Guardain Tales</h3>
             <p>Kakao Games Corp</p>
+            <div className="">
+              <span className="text-gray-500 ">
+                <GradeIcon sx={{ fontSize: 15 }} />
+              </span>
+              <span className="text-gray-500">
+                <GradeIcon sx={{ fontSize: 15 }} />
+              </span>
+              <span className="text-gray-500">
+                <GradeIcon sx={{ fontSize: 15 }} />
+              </span>
+              <span className="text-gray-500">
+                <GradeIcon sx={{ fontSize: 15 }} />{" "}
+              </span>
+              <span>
+                <GradeIcon sx={{ fontSize: 15 }} color="disabled" />
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/Dashboard/home/index.jsx
+++ src/components/pages/Dashboard/home/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./home.css";
 
 import { gameData } from "../../../../dummyData/gamesData";
 import Card from "../../../card/index";
@@ -6,9 +7,11 @@ import Card from "../../../card/index";
 const Index = () => {
   return (
     <div className="box-border p-7">
-      <div className="flex justify-between">
-        <h1>Popular Gamees and Apps</h1>
-        <button>See More</button>
+      <div className="flex justify-between py-3 border link-width border-b-green-900">
+        <h1 className="">Popular Gamees and Apps</h1>
+        <button className="px-2 py-1 text-white rounded btn-see">
+          See More
+        </button>
       </div>
       <div className="flex flex-wrap w-full gap-5">
         {gameData.map((item, i) => {
