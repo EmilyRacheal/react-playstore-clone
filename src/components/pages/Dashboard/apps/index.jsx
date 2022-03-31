7aafb9135bb364e5bb3d34fee14d534d2f82c89e
EmilyRacheal <emilychealy4@gmail.com>
2022-03-31 16:10:40 +0100

    add componet

--- src/components/pages/Dashboard/apps/index.jsx
+++ src/components/pages/Dashboard/apps/index.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import "./apps.css";
 import AppsCard from "../../../myAppCard/index";
+import { MyApps } from "../../../../secondDummy/myAppsData";
 
 const Index = () => {
   return (
-    <div className="flex justify-center w-full mt-9">
-      <div className="flex flex-col w-11/12">
+    <div className="box-border p-7">
+      <div className="">
+        <h1 className="title">My Android Apps</h1>
+      </div>
+      <div className="flex flex-wrap w-full gap-5">
+        {MyApps.map((item, i) => {
+          return <gameCard />;
+        })}
+      </div>
+      {/* <div className="flex flex-col w-11/12">
         <h1 className="title">My Android Apps</h1>
         <div className="flex flex-wrap gap-3">
           {[...Array(9)].map(() => {
             return <AppsCard />;
           })}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
