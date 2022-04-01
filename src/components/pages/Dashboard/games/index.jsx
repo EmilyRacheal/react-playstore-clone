dab805cdb7df915da41b840c73b48857b50214fa
EmilyRacheal <emilychealy4@gmail.com>
2022-04-01 15:46:12 +0100

    create component

--- src/components/pages/Dashboard/games/index.jsx
+++ src/components/pages/Dashboard/games/index.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import "./index";
+
+// import AppGame from "../../../gameCard/index";
+import { myGame } from "../../../../thirdDummyData/gameplay";
+import TheGame from "../../../gameCard/index";
 
 const Index = () => {
   return (
