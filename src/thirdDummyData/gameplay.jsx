fa61128e188af3e2ff85fed50fcddf0cfb0559db
EmilyRacheal <emilychealy4@gmail.com>
2022-04-01 15:32:09 +0100

    create  data

---
+++ src/thirdDummyData/gameplay.jsx
@@ -0,0 +1,62 @@
+import Cardss from "../assest/cardss.jpg";
+import Toon from "../assest/toon.webp";
+
+import Friend from "../assest/freids.jpg";
+import Cross from "../assest/crossword.webp";
+
+import Tech from "../assest/tech.jpg";
+import Zuma from "../assest/zuma.webp";
+
+import Vr from "../assest/vr.jpg";
+import Farm from "../assest/farm.webp;";
+
+import ManBall from "../assest/manball.jpg";
+import Football from "../assest/football.webp";
+
+import Chess from "../assest/chess.jpg";
+import Chessers from "../assest/chesser.webp";
+
+export const myGame = [
+  {
+    logo: Cardss,
+    icon: Toon,
+    title: "Toon Blast",
+    description: "Peak",
+    stars: 4,
+  },
+  {
+    logo: Friend,
+    icon: Cross,
+    title: "CrossWord Quest",
+    description: "Word Mobile",
+    stars: 4,
+  },
+  {
+    logo: Tech,
+    icon: Zuma,
+    title: "Jungle Mable Blast",
+    description: "CoolStudios",
+    stars: 4,
+  },
+  {
+    logo: Vr,
+    icon: Farm,
+    title: "Farm Heroes",
+    description: "King",
+    stars: 4,
+  },
+  {
+    logo: ManBall,
+    icon: Football,
+    title: "FIFA Soccer",
+    description: "ELECCTRONIC ART",
+    stars: 4,
+  },
+  {
+    logo: Chess,
+    icon: Chessers,
+    title: "Checkers",
+    description: "English Checkers",
+    stars: 4,
+  },
+];
